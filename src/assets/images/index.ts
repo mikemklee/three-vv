b0abff83b8432b2a6f190b57a87cc78c5577f542
Mike Lee <mikemunkyu.lee@gmail.com>
2020-04-12 18:17:14 -0400

    Rename repo to three-vv and remove obsolete files

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,7 +1,5 @@
 export default {
-  upload: require('./upload.png'),
   hand: require('./hand.png'),
-  mesh: require('./mesh.png'),
   rotate: require('./rotate.png'),
   github: require('./github.png'),
 };
